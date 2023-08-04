c87420552e6a5e621f8cb160605e3d22052d4ae2
2227500 <115985919+2227500@users.noreply.github.com>
2023-08-04 13:20:52 +0100

    Yarn spinner implemented

--- Assets/Scripts/walkPositions.cs
+++ Assets/Scripts/walkPositions.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Yarn.Unity;
 
 public class walkPositions : MonoBehaviour
 {
@@ -13,7 +14,7 @@ public class walkPositions : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        MoveToSofa();
+       // MoveToSofa();
     }
 
     // Update is called once per frame
@@ -26,4 +27,17 @@ public class walkPositions : MonoBehaviour
     {
         assistant.destination = sofa.position;
     }
+
+    public void MoveToFirePlace()
+    {
+        assistant.destination = firePlace.position;
+    }
+
+    [YarnCommand("moveToSofa")]
+    public void CharacterMover()
+    {
+        MoveToSofa();
+        MoveToFirePlace();
+    }
+
 }
